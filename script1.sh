9a2503dab28ead12e63de5688e53e969731328c5
khushi singh <khushisingh1595@gmail.com>
2026-03-29 14:43:05 +0530

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Script 1: System Identity Report
+
+STUDENT_NAME="Khushi Singh"
+SOFTWARE_CHOICE="Git"
+
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+DISTRO=$(lsb_release -d | cut -f2)
+
+echo "================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo " Software: $SOFTWARE_CHOICE"
+echo "================================"
+echo "Distribution : $DISTRO"
+echo "Kernel       : $KERNEL"
+echo "User         : $USER_NAME"
+echo "Uptime       : $UPTIME"
+echo "Date & Time  : $DATE"
+echo "License      : GNU GPL (Linux Kernel)"
