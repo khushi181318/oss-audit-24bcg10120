f230d0e8e722945d86da4b4638301a566b0ba6b2
khushi singh <khushisingh1595@gmail.com>
2026-03-30 18:59:09 +0530

    Enhance directory audit and Git config checks
    Updated the script to provide more detailed output for non-existent directories and improved Git config check.

--- script3.sh
+++ script3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Script 3: Disk and Permission Auditor
+# Author: Khushi Kumari
 
 DIRS=("/etc" "/var/log" "/home" "/usr/bin" "/tmp")
 
@@ -7,18 +8,26 @@ echo "Directory Audit Report"
 echo "----------------------"
 
 for DIR in "${DIRS[@]}"; do
- if [ -d "$DIR" ]; then
- PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
- SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
- echo "$DIR => Permissions: $PERMS | Size: $SIZE"
- else
- echo "$DIR does not exist"
- fi
+    if [ -d "$DIR" ]; then
+        PERMS=$(ls -ld $DIR | awk '{print $1, $3, $4}')
+        SIZE=$(du -sh $DIR 2>/dev/null | cut -f1)
+        echo "$DIR => Permissions: $PERMS | Size: $SIZE"
+    else
+        echo "$DIR does not exist on this system"
+    fi
 done
 
-# Check Git config
-if [ -d "$HOME/.git" ]; then
- ls -ld $HOME/.git
+echo "----------------------"
+
+# Check Git config directory
+GIT_CONFIG="$HOME/.gitconfig"
+
+if [ -f "$GIT_CONFIG" ]; then
+    PERMS=$(ls -l $GIT_CONFIG | awk '{print $1, $3, $4}')
+    echo "Git config file exists => Permissions: $PERMS"
 else
- echo "Git config directory not found"
+    echo "Git config file not found"
 fi
+
+
+
